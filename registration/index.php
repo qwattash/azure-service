08c83015745fe82ce0a3e487ca169aa8cd92994e
qwattash <mzz.lrd@gmail.com>
2013-10-18 15:42:13 +0100

    added support for companyName DB column and improved directory structure

--- registration/index.php
+++ registration/index.php
@@ -1,19 +1,8 @@
 <html>
 <head>
 <Title>Registration Form</Title>
-<style type="text/css">
-    body { background-color: #fff; border-top: solid 10px #000;
-        color: #333; font-size: .85em; margin: 20; padding: 20;
-        font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
-    }
-    h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
-    h1 { font-size: 2em; }
-    h2 { font-size: 1.75em; }
-    h3 { font-size: 1.2em; }
-    table { margin-top: 0.75em; }
-    th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
-    td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
-</style>
+<meta charset="UTF-8"/>
+<link rel="stylesheet" href="../css/default.css"/>
 </head>
 <body>
 <h1>Register here!</h1>
@@ -21,11 +10,11 @@
 <form method="post" action="index.php" enctype="multipart/form-data" >
       Name  <input type="text" name="name" id="name"/></br>
       Email <input type="text" name="email" id="email"/></br>
+      Company <input type="text" name="company" id="company"/></br>
       <input type="submit" name="submit" value="Submit" />
 </form>
 <?php
     // DB connection info
-    //TODO: Update the values for $host, $user, $pwd, and $db
     //using the values you retrieved earlier from the portal.
     $host = "eu-cdbr-azure-west-b.cloudapp.net";
     $user = "b9e0d9bc5e8435";
@@ -44,14 +33,16 @@
     try {
         $name = $_POST['name'];
         $email = $_POST['email'];
+        $company = $_POST['company'];
         $date = date("Y-m-d");
         // Insert data
-        $sql_insert = "INSERT INTO registration_tbl (name, email, date) 
-                   VALUES (?,?,?)";
+        $sql_insert = "INSERT INTO registration_tbl (name, email, date, companyName) 
+                   VALUES (?,?,?,?)";
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $name);
         $stmt->bindValue(2, $email);
         $stmt->bindValue(3, $date);
+        $stmt->bindValue(4, $company);
         $stmt->execute();
     }
     catch(Exception $e) {
@@ -72,7 +63,8 @@
         foreach($registrants as $registrant) {
             echo "<tr><td>".$registrant['name']."</td>";
             echo "<td>".$registrant['email']."</td>";
-            echo "<td>".$registrant['date']."</td></tr>";
+            echo "<td>".$registrant['date']."</td>";
+            echo "<td>".$registrant['companyName']."</td></tr>";
         }
         echo "</table>";
     } else {
