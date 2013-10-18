2a2c5c04c1cd248722ba79de11f52da46fde9b8a
qwattash <mzz.lrd@gmail.com>
2013-10-18 14:51:16 +0100

    main index added

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+//header("Location:".$_SERVER['SERVER_NAME']."/registration/index.php");
+?>
+<html>
+<head>
+<title>Redirect page</title>
+<meta charset="UTF-8"/>
+</head>
+<body>
+<a href="registration/index.php">registration</a>
+</body>
+</html>
